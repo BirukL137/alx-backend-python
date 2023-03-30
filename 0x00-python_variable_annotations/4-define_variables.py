be72efe3500ef40a6d8c2fd6814dbd3666ca8d15
BirukL137 <buraf13k@gmail.com>
2023-03-30 14:15:04 +0300

    4. Define variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Define variables
+
+Args:
+    a (int): defining a with a value of 1.
+    pi (float): defining pi with a value of 3.14.
+    i_understand_annotations (bool): defining i_understand with a value True.
+    school (str): defining school with a value of 'Holberton'
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
