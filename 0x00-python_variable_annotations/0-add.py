b0250577838848a8bbe7f27da0863a33a3761a8c
BirukL137 <buraf13k@gmail.com>
2023-03-30 13:10:36 +0300

    0. Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - add
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two float arguments and returns their results as float.
+
+    Args:
+        a (float): The first float argument.
+        b (float): The second float argument.
+
+    Returns:
+        float: The added float.
+    """
+    return a + b
