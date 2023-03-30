688eca46842e89bbae4f1ab665e548e3a1765995
BirukL137 <buraf13k@gmail.com>
2023-03-30 13:53:52 +0300

    3. Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - to string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of the float.
+
+    Args:
+        n (float): The float to take the string.
+
+    Returns:
+        str: The string of the float.
+    """
+    return str(n)
